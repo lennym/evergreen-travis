bd2b1e3fefd11688b5a2290d30938e1c98540e8d
Leonard Martin <leonard.martin@gmail.com>
2017-01-10 11:09:38 +0000

    Only add versions newer than any configured
    Do not infill old versions, for example if a user has 4,6,7 configured then do not add 5 as well.

--- test/versions.js
+++ test/versions.js
@@ -1,6 +1,7 @@
 /* eslint-env node, mocha */
 
 const versions = require('../lib/check-versions');
+const update = require('../lib/update-versions');
 const assert = require('assert');
 
 describe('version matcher', () => {
@@ -14,7 +15,7 @@ describe('version matcher', () => {
 
   it('resolves false if all versions are present', () => {
     const expected = [4, 5, 6, 7];
-    const actual = [4, 5, 7];
+    const actual = [4, 5, 6];
     return versions(expected, actual, {})
       .then(val => assert.equal(val, false));
   });
@@ -28,6 +29,29 @@ describe('version matcher', () => {
     };
     return versions(expected, actual, aliases)
       .then(val => assert.equal(val, true));
+  });
+
+  it('ignores missing versions which are older than other configured versions', () => {
+    const expected = [4, 5, 6, 7];
+    const actual = [4, 6, 7];
+    return versions(expected, actual, {})
+      .then(val => assert.equal(val, true));
+  });
+
+});
+
+describe('version updater', () => {
+
+  it('ignores missing versions which are older than other configured versions', () => {
+    const updated = [4, 5, 6, 7];
+    const existing = [4, 6, 7];
+    assert.deepEqual(update(existing, updated, {}), ['4', '6', '7']);
+  });
+
+  it('preserves aliases in configured versions', () => {
+    const updated = [4, 5, 6, 7];
+    const existing = ['lts/argon', 5, 'lts/boron'];
+    assert.deepEqual(update(existing, updated, { 'lts/argon': 4, 'lts/boron': 6 }), ['lts/argon', '5', 'lts/boron', '7']);
   });
 
 });
